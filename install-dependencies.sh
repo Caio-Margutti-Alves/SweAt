d9128fc74c2c00aacfe986facb85c249fbb7a365
Caio <caio.margutti.alves@gmail.com>
2015-10-01 15:56:29 -0300

    New Test

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -11,5 +11,9 @@ if [ ! -e $DEPS ]; then
   echo y | android update sdk -u -a -t android-23 &&
   echo y | android update sdk -u -a -t extra-android-m2repository &&
   echo y | android update sdk -u -a -t extra-google-m2repository &&
+  echo y | android update sdk --no-ui --all --filter "platform-tools" &&
+  echo y | android update sdk --no-ui --all --filter "tools" &&
+  echo y | android update sdk --no-ui --all --filter "extra-google-google_play_services" &&
+  echo y | android update sdk --no-ui --all --filter "extra-android-support" &&
   touch $DEPS
 fi
