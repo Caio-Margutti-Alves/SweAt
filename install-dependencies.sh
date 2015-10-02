1de172ac84e70523e510595d06c4f79c769e496d
Caio <caio.margutti.alves@gmail.com>
2015-10-02 00:16:38 -0300

    New Test

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -7,8 +7,8 @@ DEPS="$ANDROID_HOME/installed-dependencies"
 
 if [ ! -e $DEPS ]; then
   cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
-  echo y | android update sdk -u -a -t build-tools-22.0.1 &&
-  echo y | android update sdk -u -a -t android-22 &&
+  echo y | android update sdk -u -a -t build-tools-23.0.1 &&
+  echo y | android update sdk -u -a -t android-23 &&
   echo y | android update sdk -u -a -t extra-android-m2repository &&
   echo y | android update sdk -u -a -t extra-google-m2repository &&
   echo y | android update sdk --no-ui --all --filter "platform-tools" &&
